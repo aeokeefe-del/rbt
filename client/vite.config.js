15b5457402929af75919c14e6ada7b06600cef1e
aeokeefe-del <aeokeefe@wisc.edu>
2026-05-11 04:33:33 +0200

    debugging deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: 'rbt',
+
   plugins: [vue()],
+
   server: {
     port: 5173,
     proxy: {
